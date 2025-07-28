3b318819ada7ec91b26c530b5573c8fa14251e18
Shantela21 <shantelaslie@gmail.com>
2025-07-28 15:43:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+<script>
+    function toggleSection(sectionId) { }
+    const allSections = document.querySelectorAll('.section');
+    allSections.forEach(section = { }
+    if (section.id === sectionId) {section.style.display = section.style.display === 'block' ? 'none' : 'block'}
+    else {section.style.display = 'none'}
+    );
+
+
+    // Initially hide all sections
+    document.addEventListener("DOMContentLoaded", () = {document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+    })});
+
+
+    <script>
+        function openModal() {document.getElementById('modal').style.display = "block"}
+
+        function closeModal() {document.getElementById('modal').style.display = "none"}
+
+// Close modal when clicking outside
+        window.onclick = function(event) { }
+        const modal = document.getElementById('modal');
+        if (event.target == modal) {modal.style.display = "none"}
+
+    </script>
+</script>;
